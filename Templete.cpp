c481a5e3c1f246117b9e75d43d94fbac1b111c83
vector94 <41264619+vector94@users.noreply.github.com>
2018-10-29 20:40:20 +0600

    Add files via upload

---
+++ Templete.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll                  long long int
+#define ull                 unsigned long long int
+#define ld                  long double
+#define fi                  first
+#define se                  second
+#define pb                  push_back
+#define pbb                 pop_back
+#define mp                  make_pair
+#define all(x)              x.begin(), x.end()
+#define uniq(x)             x.erase(unique(x.begin(),x.end()),x.end())
+#define mem(array, value)   memset(array, value, sizeof(array))
+#define gcd(a,b)            __gcd(a,b)
+#define lcm(a,b)            (abs(a)/__gcd(a,b))*abs(b)
+#define PI      	        acos(-1.0)
+#define INF     	        1e9
+#define EPS     	        1e-8
+#define MOD                 1000000007
+#define MAX                 1000009
+#define line                cout << "\n==========\n"
+#define fastRead 	        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+using namespace std;
+//int dir4[4][2] = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+//int dir8[8][2] = {{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
+
+int main ()
+{
+    //freopen ( "input.txt", "r", stdin );
+    //freopen ( "output.txt", "w", stdout );
+
+    return 0;
+}
+
