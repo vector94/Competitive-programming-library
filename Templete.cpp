73fe35f798349930c49fea06399f28eb35cbf453
vector94 <asif.iqbal.3642@gmail.com>
2018-11-19 01:36:12 +0600

    Update Templete.cpp

--- Templete.cpp
+++ Templete.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 #define ll                  long long int
 #define ull                 unsigned long long int
 #define ld                  long double
@@ -20,6 +22,8 @@
 #define line                cout << "\n==========\n"
 #define fastRead 	        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> orderedSet; 
 
 //int dx4[4] = {0, -1, 0, 1};
 //int dy4[4] = {-1, 0, 1, 0};
@@ -56,7 +60,6 @@ int main ()
     //freopen ( "input.txt", "r", stdin );
     //freopen ( "output.txt", "w", stdout );
     #endif // Lollipop
-
+    
     return 0;
 }
-
