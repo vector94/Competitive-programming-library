5b12ff6af070ea1545f55f39c976c80dfd1e24ff
vector94 <asif.iqbal.3642@gmail.com>
2019-02-20 17:08:57 +0600

    Update Templete.cpp

--- Templete.cpp
+++ Templete.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 //#include <ext/pb_ds/tree_policy.hpp>
 //#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+//using namespace __gnu_pbds;
 #define ll                  long long int
 #define ull                 unsigned long long int
 #define ld                  long double
@@ -21,8 +23,17 @@
 #define EPS     	        1e-8
 #define line                cout << "\n==========\n"
 #define fastRead 	        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-using namespace std;
-//using namespace __gnu_pbds;
+#define trace(...)          __f( #__VA_ARGS__ , __VA_ARGS__ )
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+    cerr << name << " : " << arg1 << "\n";
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+    const char* comma = strchr(names + 1, ',');
+    cerr.write(names, comma - names) << " : " << arg1 << " , " ;
+    __f(comma + 1, args...);
+}
 //typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> orderedSet;
 
 //int dx[] = {1, -1, 0, 0},                  dy[] = {0, 0, 1, -1};                  // 4 Direction
@@ -72,7 +83,7 @@ inline ll bigmod(ll a, ll p, ll m){
     return res;
 }
 
-int main ()
+int main ( int argc , char const *argv[])
 {
     #ifdef Lollipop
     //freopen ("input.txt", "r", stdin);
@@ -81,3 +92,4 @@ int main ()
 
     return 0;
 }
+
