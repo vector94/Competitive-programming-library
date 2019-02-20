1200b200c29d4380946596de59b734c5a5022bcc
vector94 <asif.iqbal.3642@gmail.com>
2019-02-20 17:25:26 +0600

    Update Templete.cpp

--- Templete.cpp
+++ Templete.cpp
@@ -31,7 +31,7 @@ void __f(const char* name, Arg1&& arg1){
 template <typename Arg1, typename... Args>
 void __f(const char* names, Arg1&& arg1, Args&&... args){
     const char* comma = strchr(names + 1, ',');
-    cerr.write(names, comma - names) << " : " << arg1 << " , " ;
+    cerr.write(names, comma - names) << " : " << arg1 << " , ";
     __f(comma + 1, args...);
 }
 //typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> orderedSet;
