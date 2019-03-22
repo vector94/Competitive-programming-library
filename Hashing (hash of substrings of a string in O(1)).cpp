203577e0cd7b9727fd4f6f789901999c6941a93d
vector94 <asif.iqbal.3642@gmail.com>
2019-03-22 23:24:02 +0600

    Update Hashing (hash of substrings of a string in O(1)).cpp

--- Hashing (hash of substrings of a string in O(1)).cpp
+++ Hashing (hash of substrings of a string in O(1)).cpp
@@ -22,65 +22,7 @@ using namespace std;
 #define EPS     	        1e-8
 #define line                cout << "\n==========\n"
 #define fastRead 	        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define trace(...)          __f( #__VA_ARGS__ , __VA_ARGS__ )
-template <typename Arg1>
-void __f(const char* name, Arg1&& arg1){
-    cerr << name << " : " << arg1 << "\n";
-}
-template <typename Arg1, typename... Args>
-void __f(const char* names, Arg1&& arg1, Args&&... args){
-    const char* comma = strchr(names + 1, ',');
-    cerr.write(names, comma - names) << " : " << arg1 << " , ";
-    __f(comma + 1, args...);
-}
 //typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> orderedSet;
-
-//int dx[] = {1, -1, 0, 0},                  dy[] = {0, 0, 1, -1};                  // 4 Direction
-//int dx[] = {1, -1, 0, 0, 1, 1, -1, -1},    dy[] = {0, 0, 1, -1, 1, -1, 1, -1};    // 8 Direction
-//int dx[] = {1, -1, 1, -1, 2, 2, -2, -2},   dy[] = {2, 2, -2, -2, 1, -1, 1, -1};   // Knight Direction
-
-inline ll gcd(ll a, ll b){
-    a = abs(a);
-    b = abs(b);
-    while (b){
-        a = a % b;
-        swap (a, b);
-    }
-    return a;
-}
-inline ll power(ll a, ll p){
-    ll res = 1, x = a;
-    while (p){
-        if (p & 1){
-            res = (res * x);
-        }
-        x = (x * x);
-        p >>= 1;
-    }
-    return res;
-}
-inline ll mulmod(ll a, ll p, ll m){
-    ll res = 0, x = a%m;
-    while (p){
-        if (p & 1){
-            res = (res + x) % m;
-        }
-        x = (x << 1) % m;
-        p >>= 1;
-    }
-    return res;
-}
-inline ll bigmod(ll a, ll p, ll m){
-    ll res = 1 % m, x = a % m;
-    while (p){
-        if (p & 1){
-            res = (res * x) % m;
-        }
-        x = (x * x) % m;
-        p >>= 1;
-    }
-    return res;
-}
 
 struct Hashing {
     ll *hash1, *hash2;
