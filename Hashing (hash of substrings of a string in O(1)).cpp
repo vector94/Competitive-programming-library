322bf332318647b0e194cdf9926d02453f5304ff
vector94 <asif.iqbal.3642@gmail.com>
2019-03-22 23:39:05 +0600

    Update Hashing (hash of substrings of a string in O(1)).cpp

--- Hashing (hash of substrings of a string in O(1)).cpp
+++ Hashing (hash of substrings of a string in O(1)).cpp
@@ -28,29 +28,22 @@ struct Hashing {
     ll *hash1, *hash2;
     ll *inv1, *inv2;
     int n;
-    ll mod1 = (ll) 1e9 + 7, mod2 = (ll) 1e9 + 9;
+    ll mod1 = (ll) 1e9 + 97, mod2 = (ll) 1e9 + 9;
     ll multiplier1 = 43, multiplier2 = 31;
     ll invMultiplier1 = 441860508, invMultiplier2 = 838709685;
-
-    Hashing() {
-    }
-
+    Hashing() {}
     Hashing(string &s) {
         build_Hash(s);
     }
-
     void build_Hash(string &s) {
         n = s.size();
         hash1 = new ll[n + 1];
         hash2 = new ll[n + 1];
         inv1 = new ll[n + 1];
         inv2 = new ll[n + 1];
-
         hash1[0] = hash2[0] = 0;
         inv1[0] = inv2[0] = 1;
-
         ll p1 = 1, p2 = 1;
-
         for (int i = 0; i < n; i++) {
             hash1[i + 1] = (hash1[i] + s[i] * p1) % mod1;
             p1 = (p1 * multiplier1) % mod1;
@@ -60,20 +53,16 @@ struct Hashing {
             inv2[i + 1] = inv2[i] * invMultiplier2 % mod2;
         }
     }
-
-    ll getHash(int i, int j) { //0-based, hash of substring [i, j]
-        return getHash_2(i, j - i + 1);
+    ll get_hash(int i, int j) { //0 - based, hash of substring [i, j]
+        return get_hash2(i, j - i + 1);
     }
-
-    ll getHash_2(int i, int len) { //0- based, hash of substring [i, i+len-1]
+    ll get_hash2(int i, int len) { //0 - based, hash of substring [i, i+len-1]
         return (((hash1[i + len] - hash1[i] + mod1) * inv1[i] % mod1) << 32) 
                                 + (hash2[i + len] - hash2[i] + mod2) * inv2[i] % mod2;
     }
-
-    ll revHash(int i, int j) { //0-based
-        return getHash(n - j - 1, n - i - 1);
+    ll remains_hash(int i, int j) { //0 - based
+        return get_hash(n - j - 1, n - i - 1);
     }
-
     void clear() {
         delete (hash1);
         delete (hash2);
@@ -101,9 +90,9 @@ int main ()
     for(int i=0; i<n; i++) {
         int a, b;
         cin >> a >> b;
-        ll h = hashing.getHash(a, b);
-        ll r = hashing.revHash(a, b);
-        cout << "Hash = " << h << ", revHash = " << r << endl;
+        ll h = hashing.get_hash(a, b);
+        ll r = hashing.remains_hash(a, b);
+        cout << "get_hash = " << h << ", remains_hash = " << r << endl;
     }
     return 0;
 }
