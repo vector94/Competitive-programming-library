b7281d21cba2641a309ca2e51d38b973f1db357e
vector94 <asif.iqbal.3642@gmail.com>
2019-03-28 12:24:03 +0600

    Update DSU Using Structure.cpp

--- DSU Using Structure.cpp
+++ DSU Using Structure.cpp
@@ -1,21 +1,21 @@
 struct UnionFind {
-  vector<int> data;
-  UnionFind(int size) : data(size, -1) { }
-  bool unionSet(int x, int y) {
-    x = root(x); y = root(y);
-    if (x != y) {
-      if (data[y] < data[x]) swap(x, y);
-      data[x] += data[y]; data[y] = x;
+    vector<int> data;
+    UnionFind(int size) : data(size, -1) { }
+    bool union_set(int x, int y) {
+        x = root(x); y = root(y);
+        if (x != y) {
+            if (data[y] < data[x]) swap(x, y);
+            data[x] += data[y]; data[y] = x;
+        }
+        return x != y;
     }
-    return x != y;
-  }
-  bool findSet(int x, int y) {
-    return root(x) == root(y);
-  }
-  int root(int x) {
-    return data[x] < 0 ? x : data[x] = root(data[x]);
-  }
-  int size(int x) {
+    bool find_set(int x, int y) {
+        return root(x) == root(y);
+    }
+    int root(int x) {
+        return data[x] < 0 ? x : data[x] = root(data[x]);
+    }
+    int _size(int x) {
     return -data[root(x)];
-  }
+    }
 };
