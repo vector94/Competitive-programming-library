7a8889ed99eba5230c4011d78d9bc0d3f851340e
vector94 <asif.iqbal.3642@gmail.com>
2019-04-10 10:19:08 +0600

    Update Templete.cpp

--- Templete.cpp
+++ Templete.cpp
@@ -35,9 +35,9 @@ void __f(const char* names, Arg1&& arg1, Args&&... args){
 }
 //typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> orderedSet;
 
-//int dx[] = {1, -1, 0, 0},                  dy[] = {0, 0, 1, -1};                  // 4 Direction
-//int dx[] = {1, -1, 0, 0, 1, 1, -1, -1},    dy[] = {0, 0, 1, -1, 1, -1, 1, -1};    // 8 Direction
-//int dx[] = {1, -1, 1, -1, 2, 2, -2, -2},   dy[] = {2, 2, -2, -2, 1, -1, 1, -1};   // Knight Direction
+//int dx[] = {-1, 0, 1, 0},                  dy[] = {0, 1, 0, -1};                  // 4 Direction
+//int dx[] = {-1, 0, 1, 0, -1, 1, 1, -1},    dy[] = {0, 1, 0, -1, -1, 1, -1, 1};    // 8 Direction
+//int dx[] = {-2, -1, 1, 2, 2, -1, 1, -2},   dy[] = {1, 2, 2, 1, -1, -2, -2, -1};   // Knight Direction
 
 inline ll gcd(ll a, ll b){
     a = abs(a), b = abs(b);
