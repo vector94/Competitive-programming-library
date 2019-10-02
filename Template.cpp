dd7abec3179bd0ebd3b2a32168a8986545c6b455
vector94 <asif.iqbal.3642@gmail.com>
2019-10-02 22:19:24 +0600

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,22 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll                  long long int
-#define ull                 unsigned long long int
-#define ld                  long double
-#define fi                  first
-#define se                  second
-#define pb                  push_back
-#define pbb                 pop_back
-#define mp                  make_pair
-#define pii     	        pair<int,int>
-#define all(x)              x.begin(), x.end()
-#define uniq(x)             x.erase(unique(x.begin(),x.end()),x.end())
-#define mem(array, value)   memset(array, value, sizeof(array))
-#define PI      	        2*acos(0.0)
-#define EPS     	        1e-8
-#define line                cout << "\n==========\n"
-#define fastRead 	        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define trace(...)          __f( #__VA_ARGS__ , __VA_ARGS__ )
+#define ll                      long long int
+#define ull                     unsigned long long int
+#define ld                      long double
+#define fi                      first
+#define se                      second
+#define pb                      push_back
+#define pbb                     pop_back
+#define mp                      make_pair
+#define pii     	            pair<int,int>
+#define popcount                __builtin_popcountll
+#define all(x)                  x.begin(), x.end()
+#define mem(array, value)       memset(array, value, sizeof(array))
+#define lcm(a,b)                (abs(a) / gcd(a,b)) * abs(b)
+#define num_digit(number, base) (((long long)(log10(number) / log10(base)))+1)
+#define sqare(x)                ((x) * (x))
+#define pi      	            2 * acos(0.0)
+#define eps     	            1e-11
+#define line                    cout << "\n==========\n"
+#define fastRead 	            ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define trace(...)              __f( #__VA_ARGS__ , __VA_ARGS__ )
 template <typename Arg1>
 void __f(const char* name, Arg1&& arg1){
     cerr << name << " : " << arg1 << "\n";
@@ -28,7 +31,33 @@ void __f(const char* names, Arg1&& arg1, Args&&... args){
     __f(comma + 1, args...);
 }
 
-const int INF = 1e9;
+//int dx[] = {-1, 0, 1, 0},                  dy[] = {0, 1, 0, -1};                  // 4 Direction
+//int dx[] = {-1, 0, 1, 0, -1, 1, 1, -1},    dy[] = {0, 1, 0, -1, -1, 1, -1, 1};    // 8 Direction
+
+inline ll gcd(ll a, ll b){
+    a = abs(a), b = abs(b);
+    while (b)
+        a = a % b, swap (a, b);
+    return a;
+}
+inline ll power(ll a, ll p){
+    ll res = 1, x = a;
+    while (p){
+        if (p & 1) res = (res * x);
+        x = (x * x), p >>= 1;
+    }
+    return res;
+}
+inline ll big_mod(ll a, ll p, ll m){
+    ll res = 1 % m, x = a % m;
+    while (p){
+        if (p & 1) res = (res * x) % m;
+        x = (x * x) % m, p >>= 1;
+    }
+    return res;
+}
+
+const ll INF = 1e15;
 
 
 int main ()
@@ -37,7 +66,8 @@ int main ()
         //freopen ("input.txt", "r", stdin);
         //freopen ("output.txt", "w", stdout);
     #endif
+    //fastRead;
+
 
     return 0;
 }
-
