eeb47bdf95a469b94a4c579bd825852ed3edb36f
vector94 <asif.iqbal.3642@gmail.com>
2019-10-23 12:09:02 +0600

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -1,22 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll                      long long int
-#define ull                     unsigned long long int
-#define ld                      long double
 #define fi                      first
 #define se                      second
 #define pb                      push_back
 #define pbb                     pop_back
 #define mp                      make_pair
 #define pii     	            pair<int,int>
-#define popcount                __builtin_popcountll
 #define all(x)                  x.begin(), x.end()
 #define mem(array, value)       memset(array, value, sizeof(array))
-#define lcm(a,b)                ((abs(a) / gcd(a,b)) * abs(b))
-#define num_digit(number, base) (((long long)(log10(number) / log10(base)))+1)
-#define SQ(x)                   ((x) * (x))
-#define pi      	            (2 * acos(0.0))
-#define eps     	            1e-11
 #define line                    cout << "\n==========\n"
 #define fastRead 	            ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 #ifdef Lollipop
@@ -38,12 +30,6 @@ using namespace std;
 //int dx[] = {-1, 0, 1, 0},                  dy[] = {0, 1, 0, -1};                  // 4 Direction
 //int dx[] = {-1, 0, 1, 0, -1, 1, 1, -1},    dy[] = {0, 1, 0, -1, -1, 1, -1, 1};    // 8 Direction
 
-inline ll gcd(ll a, ll b){
-    a = abs(a), b = abs(b);
-    while (b)
-        a = a % b, swap (a, b);
-    return a;
-}
 inline ll power(ll a, ll p){
     ll res = 1, x = a;
     while (p){
