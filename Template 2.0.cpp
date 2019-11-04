680cd386e5f1faa39ac4b0d5616de5a2ef776652
vector94 <asif.iqbal.3642@gmail.com>
2019-11-04 13:49:30 +0600

    Update Template 2.0.cpp

--- Template 2.0.cpp
+++ Template 2.0.cpp
@@ -42,7 +42,7 @@ using namespace std;
     template <typename Arg1, typename... Args>
     void __f(const char* names, Arg1&& arg1, Args&&... args){
         const char* comma = strchr(names + 1, ',');
-        cerr.write(names, comma - names) << " : " << arg1 << " , ";
+        cerr.write(names, comma - names) << " = " << arg1 << " , ";
     __f(comma + 1, args...);
     }
 #else
