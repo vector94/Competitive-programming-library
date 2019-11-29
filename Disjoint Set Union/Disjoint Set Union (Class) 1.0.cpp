3e36c1bed9bee6685dde8cac30e3dea8e2377ff2
vector94 <asif.iqbal.3642@gmail.com>
2019-11-29 06:00:02 +0600

    Update Disjoint Set Union (Class) 1.0.cpp

--- Disjoint Set Union/Disjoint Set Union (Class) 1.0.cpp
+++ Disjoint Set Union/Disjoint Set Union (Class) 1.0.cpp
@@ -5,6 +5,7 @@ private:
     int group;                              /// total disconnected group;
     int sz[nax];                            /// size of a network
     int parent[nax];                        /// parent of a node
+    int edge_cnt[nax];                      /// number of edge;
 public:
     inline void Init(int n){
         this -> n = group = n;
@@ -27,20 +28,31 @@ public:
     inline int size(int p){                 /// gives the size of the group of p
         return sz[ find(p) ];
     }
+    inline int edge(int p){                 /// gives total number of edges in network
+        return edge_cnt[find(p)];
+    }
     inline void merge(int p, int q){        /// merges the group of p and q
         int par1 = find(p);
         int par2 = find(q);
+        edge_cnt[par1]++;
         if(par1 == par2){
+            if (p == q){
+                edge_cnt[par1]++;
+            }
             return;
         }
         else{
             group--;
         }
         if(sz[par1] > sz[par2]){
-            parent[par2] = par1, sz[par1] += sz[par2];
+            parent[par2] = par1;
+            sz[par1] += sz[par2];
+            edge_cnt[par1] += edge_cnt[par2];
         }
         else{
-            parent[par1] = par2, sz[par2] += sz[par1];
+            parent[par1] = par2;
+            sz[par2] += sz[par1];
+            edge_cnt[par2] += edge_cnt[par1];
         }
     }
 }DSU;
