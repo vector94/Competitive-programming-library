ffa41e3d5d0880828302c9743d9b01e0ed4976a5
vector94 <asif.iqbal.3642@gmail.com>
2019-12-07 11:44:17 +0600

    Update Sublime_Template.cpp

--- Sublime_Template.cpp
+++ Sublime_Template.cpp
@@ -10,7 +10,7 @@ using namespace std;
 #define all(x)                  x.begin(), x.end()
 #define mem(array, value)       memset(array, value, sizeof(array))
 #define fastRead 	            ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-
+#define line                    cout << "\n===================\n"
 
 int main ()
 {
