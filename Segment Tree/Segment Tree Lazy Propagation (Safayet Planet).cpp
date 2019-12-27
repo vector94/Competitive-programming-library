8aa636e4e11b099f906f85a0c70963b4de42bd8b
vector94 <asif.iqbal.3642@gmail.com>
2019-12-27 12:08:18 +0600

    Update Segment Tree Lazy Propagation (Safayet Planet).cpp

--- Segment Tree/Segment Tree Lazy Propagation (Safayet Planet).cpp
+++ Segment Tree/Segment Tree Lazy Propagation (Safayet Planet).cpp
@@ -9,7 +9,7 @@ struct info
     ll prop, sum;
 } tree[nax * 3];
 
-void init(int node, int b, int e) {
+void build(int node, int b, int e) {
     if(b == e) {
         tree[node].sum = ara[b];
         return;
@@ -17,25 +17,9 @@ void init(int node, int b, int e) {
     int left = node << 1;
     int right = (node << 1) + 1;
     int mid = (b + e) >> 1;
-    init(left, b, mid);
-    init(right, mid + 1, e);
+    build(left, b, mid);
+    build(right, mid + 1, e);
     tree[node].sum = tree[left].sum + tree[right].sum;
-}
-
-ll query(int node, ll b, ll e, int i, int j, ll carry = 0)
-{
-    if (i > e || j < b){
-        return 0;
-    }
-    if (b >= i && e <= j){
-        return tree[node].sum + carry * (e - b + 1);
-    }
-    int left = node << 1;
-    int right = (node << 1) + 1;
-    int mid = (b + e) >> 1;
-    ll p1 = query(left, b, mid, i, j, carry + tree[node].prop);
-    ll p2 = query(right, mid+1, e, i, j, carry + tree[node].prop);
-    return p1 + p2;
 }
 
 void update(int node, ll b, ll e, int i, int j, int x)
@@ -56,6 +40,22 @@ void update(int node, ll b, ll e, int i, int j, int x)
     tree[node].sum = tree[left].sum + tree[right].sum + (e - b + 1) * tree[node].prop;
 }
 
+ll query(int node, ll b, ll e, int i, int j, ll carry = 0)
+{
+    if (i > e || j < b){
+        return 0;
+    }
+    if (b >= i && e <= j){
+        return tree[node].sum + carry * (e - b + 1);
+    }
+    int left = node << 1;
+    int right = (node << 1) + 1;
+    int mid = (b + e) >> 1;
+    ll p1 = query(left, b, mid, i, j, carry + tree[node].prop);
+    ll p2 = query(right, mid+1, e, i, j, carry + tree[node].prop);
+    return p1 + p2;
+}
+
 int main ()
 {
     int n;
@@ -63,7 +63,22 @@ int main ()
     for (int i = 1; i <= n; i++){
         cin >> ara[i];
     }
-    init(1, 1, n);
-
+    build(1, 1, n);         /// 1 - based
+    int q;
+    cin >> q;
+    while (q--){
+        int type;
+        cin >> type;
+        if (type == 1){
+            int l, r, x;
+            cin >> l >> r >> x;
+            update(1, 1, n, l, r, x);
+        }
+        else{
+            int l, r;
+            cin >> l >> r;
+            cout << query(1, 1, n, l, r) << endl;
+        }
+    }
     return 0;
 }
