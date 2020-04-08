38760715e4a2728cdae73bde3b0746c055137976
vector94 <asif.iqbal.3642@gmail.com>
2020-04-08 18:14:15 +0600

    Add files via upload

---
+++ 1011 - Marriage Ceremonies.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                      long long int
+#define fi                      first
+#define se                      second
+#define pb                      push_back
+#define pbb                     pop_back
+#define mp                      make_pair
+#define pii     	            pair<int,int>
+#define all(x)                  x.begin(), x.end()
+#define mem(array, value)       memset(array, value, sizeof(array))
+#define fastRead 	            ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#ifdef Lollipop
+    #define line                    cout << "\n===================\n"
+    #define trace(...)              __f( #__VA_ARGS__ , __VA_ARGS__ )
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cerr << name << " = " << arg1 << "\n";
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ',');
+        cerr.write(names, comma - names) << " = " << arg1 << " , ";
+    __f(comma + 1, args...);
+    }
+#else
+    #define line
+    #define trace(...)
+#endif
+
+int dp[20][(1 << 16) + 5], mark[20][(1 << 16) + 5];
+int a[20][20];
+int n, cs;
+
+int solve(int i, int mask)
+{
+    if (i == n){
+        return 0;
+    }
+    int &ret = dp[i][mask];
+    if (mark[i][mask] == cs){
+        return ret;
+    }
+    mark[i][mask] = cs;
+    ret = 0;
+    for (int j = 0; j < n; j++){
+        if (!(mask & (1 << j))){
+            ret = max(ret, a[i][j] + solve(i + 1, mask | (1 << j)));
+        }
+    }
+    return ret;
+}
+
+int main ()
+{
+    #ifdef Lollipop
+        freopen ("input.txt", "r", stdin);
+        //freopen ("output.txt", "w", stdout);
+    #endif
+    //fastRead;
+    int t;
+    cin >> t;
+    for (cs = 1; cs <= t; cs++){
+        cin >> n;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                cin >> a[i][j];
+            }
+        }
+        printf ("Case %d: %d\n", cs, solve(0, 0));
+    }
+    return 0;
+}
+
+
