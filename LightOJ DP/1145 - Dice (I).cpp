0cafb882dbf9a696981c5308bf1036a85ed6bab9
vector94 <asif.iqbal.3642@gmail.com>
2020-04-28 01:03:58 +0600

    Add files via upload

---
+++ LightOJ DP/1145 - Dice (I).cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                      long long int
+#define fi                      first
+#define se                      second
+#define pb                      push_back
+#define pbb                     pop_back
+#define mp                      make_pair
+#define pii     	            pair<int,int>
+#define all(x)                  x.begin(), x.end()
+#define mem(array, value)       memset(array, value, sizeof(array))
+#define endl                    "\n"
+#define fastRead 	            ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#ifdef Lollipop
+    #define line                    cout << "\n===================\n"
+    #define trace(...)              __f( #__VA_ARGS__ , __VA_ARGS__ )
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cerr << name << " = " << arg1 << "\n";
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ',');
+        cerr.write(names, comma - names) << " = " << arg1 << " , ";
+    __f(comma + 1, args...);
+    }
+#else
+    #define line
+    #define trace(...)
+#endif
+
+const int mod = 100000007;
+int dp[2][15005];
+
+int main ()
+{
+    #ifdef Lollipop
+        freopen ("input.txt", "r", stdin);
+        //freopen ("output.txt", "w", stdout);
+    #endif
+    fastRead;
+    int t;
+    cin >> t;
+    for (int cs = 1; cs <= t; cs++){
+        int n, k, s;
+        cin >> n >> k >> s;
+        mem(dp, 0);
+        for (int i = 1; i <= k; i++){
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i <= n; i++){
+            int cur = i % 2;
+            int pre = cur ^ 1;
+            for (int j = 1; j <= s; j++){
+                dp[cur][j] = dp[cur][j - 1] + dp[pre][j - 1] - dp[pre][max(0, j - k - 1)];
+                dp[cur][j] = (dp[cur][j] + mod) % mod;
+            }
+        }
+        printf ("Case %d: %d\n", cs, dp[n % 2][s]);
+    }
+    return 0;
+}
+
