dfd53e71b3c1c536eed9b9db3fb61975440f26e4
vector94 <asif.iqbal.3642@gmail.com>
2020-05-05 13:32:25 +0600

    Update 946D Timetable.cpp

--- Codeforces DP/946D Timetable.cpp
+++ Codeforces DP/946D Timetable.cpp
@@ -28,26 +28,25 @@ using namespace std;
     #define trace(...)
 #endif
 
-const int inf = 1e8;
-const int nax = 505;
 int n, m, k;
-int need[nax][nax];
-int dp[nax][nax];
-int l[nax], r[nax];
-string ara[nax];
+vector<int> a[505];
+int cnt[505][505];
+int dp[505][505];
 
-int solve(int day, int rem)
+int solve(int pos, int rem)
 {
-    if (day > n){
+    if (pos == n){
         return 0;
     }
-    int &ret = dp[day][rem];
+    int &ret = dp[pos][rem];
     if (ret != -1){
         return ret;
     }
-    ret = inf;
-    for (int j = m; j >= 0 && need[day][j] <= rem; j--){
-        ret = min(ret, j + solve(day + 1, rem - need[day][j]));
+    ret = INT_MAX;
+    for (int i = 0; i <= a[pos].size(); i++){
+        if (rem >= i){
+            ret = min(ret, cnt[pos][i] + solve(pos + 1, rem - i));
+        }
     }
     return ret;
 }
@@ -55,34 +54,31 @@ int solve(int day, int rem)
 int main ()
 {
     #ifdef Lollipop
-        freopen ("in1.txt", "r", stdin);
-        //freopen ("ans1.txt", "w", stdout);
+        freopen ("input.txt", "r", stdin);
+        //freopen ("output.txt", "w", stdout);
     #endif
-    fastRead;
+    //fastRead;
     cin >> n >> m >> k;
-    for (int i = 1; i <= n; i++){
-        cin >> ara[i];
-    }
-    for (int i = 1; i <= n; i++){
+    for (int i = 0; i < n; i++){
+        string s;
+        cin >> s;
         for (int j = 0; j < m; j++){
-            l[j + 1] = l[j] + (ara[i][j] - '0');
-        }
-        for (int j = m - 1; j >= 0; j--){
-            r[j + 1] = r[j + 2] + (ara[i][j] - '0');
-        }
-        int total = l[m];
-        need[i][0] = total;
-        for (int j = 1; j <= m; j++){
-            need[i][j] = inf;
-            for (int p = 1; p <= m - j + 1; p++){
-                need[i][j] = min(need[i][j], total - (l[p + j - 1] - l[p - 1]));
-            }
-            for (int p = m; p >= j; p--){
-                need[i][j] = min(need[i][j], total - (r[p - j + 1] - r[p + 1]));
+            if (s[j] == '1'){
+                a[i].pb(j);
             }
         }
+        for (int j = 0; j < a[i].size(); j++){
+            cnt[i][j] = INT_MAX;
+            for (int k = 0; k <= j; k++){
+                int l = k;
+                int r = a[i].size() - (j - k) - 1;
+                cnt[i][j] = min(cnt[i][j], a[i][r] - a[i][l] + 1);
+            }
+        }
+        cnt[i][a[i].size()] = 0;
     }
     mem(dp, -1);
-    cout << solve(1, k) << endl;
+    cout << solve(0, k) << endl;
     return 0;
 }
+ 
