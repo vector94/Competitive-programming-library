7680cc916fa336b312d8fc2ab0cebd3663b53308
vector94 <asif.iqbal.3642@gmail.com>
2020-10-30 18:50:04 +0600

    Add files via upload

---
+++ Sparse Table/Range Minimum Queries (RMQ).cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll                      long long int
+#define fi                      first
+#define se                      second
+#define pb                      push_back
+#define pbb                     pop_back
+#define mp                      make_pair
+#define pii     	            pair<int,int>
+#define all(x)                  x.begin(), x.end()
+#define mem(array, value)       memset(array, value, sizeof(array))
+#define endl                    "\n"
+#define fastRead 	            ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#ifdef Lollipop
+    #define line                    cout << "\n===================\n"
+    #define trace(...)              __f( #__VA_ARGS__ , __VA_ARGS__ )
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cerr << name << " = " << arg1 << "\n";
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ',');
+        cerr.write(names, comma - names) << " = " << arg1 << " , ";
+    __f(comma + 1, args...);
+    }
+#else
+    #define line
+    #define trace(...)
+#endif
+
+const int nax = 1e5 + 5;
+int sparse_table[nax][25], log[nax];
+int ara[nax];
+int n;
+
+void build()
+{
+    log[1] = 0;
+    for (int i = 2; i < nax; i++){
+        log[i] = log[i / 2] + 1;
+    }
+    for (int i = 0; i < n; i++){
+        sparse_table[i][0] = ara[i];
+    }
+    for (int j = 1, j <= 25; j++){
+        for (int i = 0; i + (1 << j) <= n; i++){
+            sparse_table[i][j] = min(sparse_table[i][j - 1], sparse_table[i + (1 << (j - 1))][j - 1]);
+        }
+    }
+}
+
+ll query(int x, int y)
+{
+    int j = log[y - x + 1];
+    return min(sparse_table[x][j], sparse_table[r - (1 << j) + 1][j]);
+}
+
+int main ()
+{
+    #ifdef Lollipop
+        //freopen ("input.txt", "r", stdin);
+        //freopen ("output.txt", "w", stdout);
+    #endif
+    fastRead;
+
+    return 0;
+}
+
