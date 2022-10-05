394a2491a3198aa48c465e33ef8f9ff00a661700
Asif Ahmed <asif.ahmed9414@gmail.com>
2022-10-05 21:06:09 +0600

    Add files via upload

---
+++ Online Judge/LeetCode/75. Sort Colors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int lo = 0;
+        int hi = nums.size() - 1;
+        int mid = 0;
+
+        while (mid <= hi){
+            if (nums[mid] == 0){
+                swap(nums[lo], nums[mid]);
+                lo++, mid++;
+            }
+            else if (nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[hi]);
+                hi--;
+            }
+        }
+    }
+};
+
+// Dutch National Flag Algorithm
